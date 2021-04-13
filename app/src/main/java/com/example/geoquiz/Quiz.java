6c4f54fe43658c2f95169c7759cd23c2f9bd301d
KyleChutjian <kyle.chutjian@gmail.com>
2021-04-13 14:59:24 -0400

    Added image asynctask

--- app/src/main/java/com/example/geoquiz/Quiz.java
+++ app/src/main/java/com/example/geoquiz/Quiz.java
@@ -1,15 +1,23 @@
 package com.example.geoquiz;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -40,6 +48,7 @@ public class Quiz extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
 
@@ -48,6 +57,11 @@ public class Quiz extends Fragment {
         View view =  inflater.inflate(R.layout.fragment_quiz, container, false);
         timer = (TextView) view.findViewById(R.id.timeElapsedTitle);
         runTimer(timer);
+
+//        ImageView image = (ImageView) view.findViewById(R.id.StateImage);
+        new DownloadImageTask((ImageView) view.findViewById(R.id.StateImage))
+                .execute("https://suncatcherstudio.com/uploads/patterns/us-states/map-outlines/svg/hawaii-map-outline-dddddd.png");
+
         return view;
     }
 
@@ -62,5 +76,36 @@ public class Quiz extends Fragment {
                 timer.setText("Time's Up!");
             }
         }.start();
+    }
+
+    public boolean isCorrect(View view) {
+        System.out.println(view.getId());
+
+        return false;
+    }
+
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
